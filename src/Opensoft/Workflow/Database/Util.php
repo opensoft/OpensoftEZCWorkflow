2bbb5a5796da44d9387ff13c43f6551ba9b1cfb0
Moisés Maciá <moises.macia@ideup.com>
2012-04-03 17:32:53 +0200

    added database serializer/unserializer from original Zeta Components project

---
+++ src/Opensoft/Workflow/Database/Util.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Opensoft\Workflow\Database;
+
+/**
+ * File containing the ezcWorkflowDatabaseUtil class.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * @package WorkflowDatabaseTiein
+ * @version //autogen//
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @access private
+ */
+
+/**
+ * Utility methods for WorkflowDatabaseTiein.
+ *
+ * @package WorkflowDatabaseTiein
+ * @version //autogen//
+ * @access private
+ */
+abstract class Util
+{
+    /**
+     * Wrapper for serialize() that returns an empty string
+     * for empty arrays and null values.
+     *
+     * @param mixed $var
+     * @return string
+     */
+    public static function serialize($var)
+    {
+        $var = serialize($var);
+
+        if ($var == 'a:0:{}' || $var == 'N;') {
+            return '';
+        }
+
+        return $var;
+    }
+
+    /**
+     * Wrapper for unserialize().
+     *
+     * @param string $serializedVar
+     * @param mixed $defaultValue
+     * @return mixed
+     */
+    public static function unserialize($serializedVar, $defaultValue = array())
+    {
+        if (!empty( $serializedVar)) {
+            return unserialize($serializedVar);
+        } else {
+            return $defaultValue;
+        }
+    }
+}
